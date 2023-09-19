ad0015713d2a66112da79878e3c2a0bc104dad57
Andrada Serban <andi.andrada@gmail.com>
2023-09-19 23:41:24 +0200

    Fixed issue #1

--- src/main/java/hu/bme/mit/spaceship/GT4500.java
+++ src/main/java/hu/bme/mit/spaceship/GT4500.java
@@ -78,7 +78,7 @@ public class GT4500 implements SpaceShip {
 
       case ALL:
         // try to fire both of the torpedo stores
-        //TODO implement feature
+       firingSuccess = true;
 
         break;
     }
